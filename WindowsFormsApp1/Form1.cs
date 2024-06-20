b2fb538555176c6a1d4432a8014be199da254f22
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-06-19 21:17:26 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(textBox1.Text, out float lado))
+            {
+                float area = lado * lado;
+
+                lblResultado.Text = "A área do quadrado é: " + area;
+            }
+            else
+            {
+                MessageBox.Show("Entrada invalida. Por favor, digite um número válido.");
+            }
+        }
+    }
+}
